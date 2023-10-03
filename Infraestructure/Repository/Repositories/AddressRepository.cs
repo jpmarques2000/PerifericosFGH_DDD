f51950d5cf73bf2419623adce7e4b234a6dca2ed
Joao Paulo <jpmarques2000@hotmail.com>
2023-10-03 15:35:32 -0300

    Verificar cep já existente

--- Infraestructure/Repository/Repositories/AddressRepository.cs
+++ Infraestructure/Repository/Repositories/AddressRepository.cs
@@ -32,6 +32,13 @@ namespace Infraestructure.Repository.Repositories
 
             try
             {
+                var verifyExists = await _context.Address.FirstOrDefaultAsync(a => a.Cep == newAddress.Cep);
+                if (verifyExists != null)
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Endereço com esse cep já existente";
+                    return serviceResponse;
+                }
                 _context.Address.Add(address);
                 await _context.SaveChangesAsync();
 
