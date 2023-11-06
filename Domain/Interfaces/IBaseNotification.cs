a7fb75aa608c39d36f54e6eae8019bfa23d68ca7
Joao Paulo <jpmarques2000@hotmail.com>
2023-11-06 15:30:21 -0300

    Add ControllerBase with notifications

---
+++ Domain/Interfaces/IBaseNotification.cs
@@ -0,0 +1,16 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IBaseNotification
+    {
+        IReadOnlyCollection<Notification> Notifications { get; }
+        void AddNotifications(IReadOnlyCollection<Notification> notifications);
+        bool IsValid { get; }
+    }
+}
---
+++ Domain/Services/Result/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.Result
+{
+    public record Result(HttpStatusCode StatusCode, Object Data);
+    public record ResultPagination(HttpStatusCode StatusCode, Object Data, Object Pagination);
+}
